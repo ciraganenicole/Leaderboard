27b744819a9e2f0ae2cb30cc7fd8da8ad1d8573e
Ciragane Nicole <ciraganenicole@gmail.com>
2022-04-07 17:09:16 +0200

    Implement the add and  refresh feature using API

--- src/index.js
+++ src/index.js
@@ -1,40 +1,8 @@
 import './style.css';
-import Player from '../modules/items.js';
+import Players from '../modules/items';
 
-class Players {
-  constructor() {
-    this.players = [];
-  }
-
-  initialize() {
-    const dataString = localStorage.getItem('playerData');
-    if (dataString) {
-      this.players = JSON.parse(dataString).map((player) => new Player(player.name, player.score));
-      this.setHtml();
-    }
-  }
-
-  newPlayer(name, score) {
-    const player = new Player(name, score);
-    this.players.push(player);
-    localStorage.setItem('playerData', JSON.stringify(this.players));
-    this.setHtml();
-    return player;
-  }
-
-  getPlayerList() {
-    let containerHtml = '';
-    this.players.forEach((player) => {
-      containerHtml += player.getHtml();
-    });
-    return containerHtml;
-  }
-
-  setHtml() {
-    const container = document.querySelector('.container-list');
-    container.innerHTML = this.getPlayerList();
-  }
-}
-const playerRepo = new Players();
-
-playerRepo.initialize();
+const players = new Players();
+document.querySelector('.refresh').addEventListener('click', Players.refreshScore);
+document.querySelector('#list-form')
+  .addEventListener('submit', players.addPlayer);
+document.addEventListener('DOMContentLoaded', Players.refreshScore);
